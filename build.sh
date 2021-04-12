6338e3bf6a01fa4e2283ee78630d8034d1e1620f
Irene Vagionakis <irenevagionakis@outlook.com>
2021-04-12 18:46:01 +0200

    only xml data for release

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export ANT_OPTS='-XX:PermSize=64m -XX:MaxPermSize=128m -Xmx512m -Dinfo.aduna.platform.appdata.basedir=./webapps/openrdf-sesame/app_dir/ -Dorg.eclipse.jetty.LEVEL=WARN'
-
-sw/ant/bin/ant -S -f local.build.xml $*
