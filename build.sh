579196e8d7ac5e23d109e518c06ee64e7590368b
IreneVagionakis <11628725+IreneVagionakis@users.noreply.github.com>
2026-06-29 12:52:01 +0200

    changed build.sh params

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export ANT_OPTS='-XX:PermSize=64m -XX:MaxPermSize=128m -Xmx512m -Dinfo.aduna.platform.appdata.basedir=./webapps/openrdf-sesame/app_dir/ -Dorg.eclipse.jetty.LEVEL=WARN'
+export ANT_OPTS='-Xms64m -Xmx512m -XX:MaxMetaspaceSize=128m -Dinfo.aduna.platform.appdata.basedir=./webapps/openrdf-sesame/app_dir/ -Dorg.eclipse.jetty.LEVEL=WARN'
 
 sw/ant/bin/ant -S -f local.build.xml $*
